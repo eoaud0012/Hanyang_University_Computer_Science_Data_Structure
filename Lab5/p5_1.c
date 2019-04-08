4b7447b14e22547adc0bf4c6fe257d952288ec55
2014017710 <2014017710@hanyang.ac.kr>
2019-04-08 05:53:13 -0700

    20190404Assignment

--- Lab5/p5_1.c
+++ Lab5/p5_1.c
@@ -1,65 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct CompleteTree 
+struct CompleteTree 
 {
 	int Size;
 	int nodeNum;
 	int* Element;
 };
-
 typedef struct CompleteTree* Tree;
-
 Tree CreateTree(int treeSize) {
-	Tree tree = NULL;
-	tree = (Tree)malloc(sizeof(Tree));
-	tree->Size = treeSize+1;
-	tree->nodeNum = 1;
-	if(tree == NULL) {
-		printf("Out Of Memory!");
-		exit(1);
-	}
-	tree->Element = malloc(sizeof(int)*treeSize);
-	if(tree->Element == NULL) {
-		printf("Out Of Memory!");
-		exit(1);
-	}
-	return tree;	
+	Tree t;
+	t = (Tree)malloc(sizeof(Tree));
+	t->Size = treeSize;
+	t->nodeNum = 1;
+	t->Element = (int*)malloc(sizeof(int)*t->Size);
+return t;
 }
 void Insert(Tree tree, int value) {
-	if(tree->nodeNum >= tree->Size) {
+	if(tree->Size < tree->nodeNum)
 		printf("Tree is Full\n");
-		return;
-	}
-		tree->Element[tree->nodeNum] = value;
-		tree->nodeNum++;	
+	else tree->Element[tree->nodeNum++] = value;
 }
-
 void printPreorder(Tree tree, int index) {
-	printf("%d ", tree->Element[index]);
-	
-	if(tree->Element[index*2] && (index*2) < (tree->Size-1)) {
-	printPreorder(tree, index*2);
-	}
-	if(tree->Element[(index*2)+1] && (index*2+1)<(tree->Size-1)) {
-	printPreorder(tree, (index*2)+1);
+	if(index < tree->nodeNum) {
+		printf("%d ", tree->Element[index]);
+		printPreorder(tree, index*2);
+		printPreorder(tree, index*2+1);
 	}
 }
-
-//void printInorder(Tree tree, int index);
-//void printPostorder(Tree tree, int index);
+void printInorder(Tree tree, int index) {
+	if(index < tree->nodeNum) {
+		printInorder(tree, index*2);
+		printf("%d ", tree->Element[index]);
+		printInorder(tree, index*2+1);
+	}
+} 
+void printPostorder(Tree tree, int index) {
+	if(index < tree->nodeNum) {
+		printPostorder(tree, index*2);
+		printPostorder(tree, index*2+1);
+		printf("%d ", tree->Element[index]);
+	}
+}
 
 void printTree(Tree tree) {
 	printf("Preorder : ");
 	printPreorder(tree, 1);
-	printf("\t");
-//	printInorder(tree, 0);
-//	printPostorder(tree, 0);
+	printf("\n");
+//	printf("Inorder : ");
+//	printInorder(tree, 1);
+//	printf("\n");
+//	printf("Postorder : ");
+//	printPostorder(tree, 1); 
+//	printf("\n");
 }
 void freeTree(Tree tree) {
 	free(tree->Element);
 	free(tree);
 }
-
 void main(int argc, char* argv[])
 {
 	FILE *fi;
---
+++ Lab5/p5_2.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct CompleteTree 
+{
+	int Size;
+	int nodeNum;
+	int* Element;
+};
+typedef struct CompleteTree* Tree;
+Tree CreateTree(int treeSize) {
+	Tree t;
+	t = (Tree)malloc(sizeof(Tree));
+	t->Size = treeSize;
+	t->nodeNum = 1;
+	t->Element = (int*)malloc(sizeof(int)*t->Size);
+return t;
+}
+void Insert(Tree tree, int value) {
+	if(tree->Size < tree->nodeNum)
+		printf("Tree is Full\n");
+	else tree->Element[tree->nodeNum++] = value;
+}
+void printPreorder(Tree tree, int index) {
+	if(index < tree->nodeNum) {
+		printf("%d ", tree->Element[index]);
+		printPreorder(tree, index*2);
+		printPreorder(tree, index*2+1);
+	}
+}
+void printInorder(Tree tree, int index) {
+	if(index < tree->nodeNum) {
+		printInorder(tree, index*2);
+		printf("%d ", tree->Element[index]);
+		printInorder(tree, index*2+1);
+	}
+} 
+void printPostorder(Tree tree, int index) {
+	if(index < tree->nodeNum) {
+		printPostorder(tree, index*2);
+		printPostorder(tree, index*2+1);
+		printf("%d ", tree->Element[index]);
+	}
+}
+
+void printTree(Tree tree) {
+	printf("Preorder : ");
+	printPreorder(tree, 1);
+	printf("\n");
+	printf("Inorder : ");
+	printInorder(tree, 1);
+	printf("\n");
+	printf("Postorder : ");
+	printPostorder(tree, 1); 
+	printf("\n");
+}
+void freeTree(Tree tree) {
+	free(tree->Element);
+	free(tree);
+}
+void main(int argc, char* argv[])
+{
+	FILE *fi;
+	Tree tree;
+	int treeSize;
+	int tempNum;
+
+	fi = fopen(argv[1], "r");
+	fscanf(fi, "%d", &treeSize);
+	tree = CreateTree(treeSize);
+	while(fscanf(fi, "%d", &tempNum) == 1)
+	{
+		Insert(tree, tempNum);
+	}
+	printTree(tree);
+	freeTree(tree);
+}
